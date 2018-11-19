78087b5b18408243fbd30328ba0df1e2796d3020
Wander Fernandes <wandersf95@gmail.com>
2018-11-18 22:19:54 -0200

    Criando Componente Usando ControlValueAccessor (Parte 2)

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -1,22 +1,43 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
 import { RadioOption } from './radio-option.model';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
     selector: 'mt-radio',
-    templateUrl: './radio.component.html'
+    templateUrl: './radio.component.html',
+    providers: [{
+        provide: NG_VALUE_ACCESSOR,
+        useExisting: forwardRef(() => RadioComponent),
+        multi: true
+    }]
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements OnInit, ControlValueAccessor {
 
     @Input() public options: RadioOption[];
 
     public value: any;
 
+    public onChange: any;
+
     constructor() { }
 
-    ngOnInit() {
+    public ngOnInit() {
     }
 
     public setValue(value: any): void {
         this.value = value;
+        this.onChange(this.value);
     }
+
+    public writeValue(obj: any): void {
+        this.value = obj;
+    }
+
+    public registerOnChange(fn: any): void {
+        this.onChange = fn;
+    }
+
+    public registerOnTouched(fn: any): void { }
+
+    public setDisabledState?(isDisabled: boolean): void { }
 }
