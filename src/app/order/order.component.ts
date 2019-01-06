746633b8402a9bd81ac12a61836044d22a4ead5a
Wander Fernandes <wandersf95@gmail.com>
2019-01-05 23:08:46 -0200

    Criando Validadores Personalizados

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,7 +4,7 @@ import { RadioOption } from 'app/shared/radio/radio-option.model';
 import { OrderService } from './order.service';
 import { Order, OrderItem } from './order.model';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
     selector: 'mt-order',
@@ -20,6 +20,20 @@ export class OrderComponent implements OnInit {
 
     numberPattern = /^[0-9]*$/;
 
+    private static equalsTo(group: AbstractControl): {[key: string]: boolean} {
+        const email = group.get('email');
+        const emailConfirmation = group.get('emailConfirmation');
+
+        if (!email || !emailConfirmation) {
+            return undefined;
+        }
+
+        if (email.value !== emailConfirmation.value) {
+            return { emailsNotMatch: true };
+        }
+        return undefined;
+    }
+
     constructor(private orderService: OrderService, private router: Router, private formBuilder: FormBuilder) { }
 
     ngOnInit() {
@@ -31,7 +45,7 @@ export class OrderComponent implements OnInit {
             number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
             optionalAddress: this.formBuilder.control(''),
             paymentOption: this.formBuilder.control('', [Validators.required])
-        });
+        }, { validator: OrderComponent.equalsTo });
     }
 
     public getRadioOption(): RadioOption[] {
